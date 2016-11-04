aa8a19ec6a43c9b80f9ef464b51ea90f7cd0055b
Erick Bogarin <erick.bogarin@gmail.com>
2016-11-04 09:29:28 -0400

    Add server-side validation

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import timezones from '../../data/timezones';
 import map from 'lodash/map';
+import classnames from 'classnames';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -11,7 +12,9 @@ class SignupForm extends React.Component {
       email: '',
       password: '',
       passwordConfirmation: '',
-      timezone: ''
+      timezone: '',
+      errors: {},
+      isLoading: false
     }
 
     this.onChange = this.onChange.bind(this);
@@ -19,6 +22,7 @@ class SignupForm extends React.Component {
   }
 
   render() {
+    const { errors } = this.state;
     const options = map(timezones, (val, key) =>
       <option key={val} value={val}x>{key}</option>
     );
@@ -26,7 +30,7 @@ class SignupForm extends React.Component {
       <form onSubmit={this.onSubmit}>
         <h1>Join our community!</h1>
 
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.username })}>
           <label className="control-label">Username</label>
           <input
             value={this.state.username}
@@ -35,9 +39,10 @@ class SignupForm extends React.Component {
             name="username"
             className="form-control"
           />
+        {errors.username && <span className="help-block">{errors.username}</span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.email })}>
           <label className="control-label">Email</label>
           <input
             value={this.state.email}
@@ -46,9 +51,10 @@ class SignupForm extends React.Component {
             name="email"
             className="form-control"
           />
+        {errors.email && <span className="help-block">{errors.email}</span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.password })}>
           <label className="control-label">Password</label>
           <input
             value={this.state.password}
@@ -57,9 +63,10 @@ class SignupForm extends React.Component {
             name="password"
             className="form-control"
           />
+          {errors.password && <span className="help-block">{errors.password}</span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.passwordConfirmation })}>
           <label className="control-label">Password Confirmation</label>
           <input
             value={this.state.passwordConfirmation}
@@ -68,9 +75,10 @@ class SignupForm extends React.Component {
             name="passwordConfirmation"
             className="form-control"
           />
+          {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
         </div>
 
-        <div className="form-group">
+        <div className={classnames("form-group", { 'has-error': errors.timezone })}>
           <label className="control-label">Timezone</label>
           <select
             value={this.state.timezone}
@@ -81,10 +89,11 @@ class SignupForm extends React.Component {
             <option value="" disabled>Choose Your Timezone</option>
             {options}
           </select>
+          {errors.timezone && <span className="help-block">{errors.timezone}</span>}
         </div>
 
         <div className="form-group">
-          <button className="btn btn-primary btn-lg">
+          <button disabled={ this.state.isLoading } className="btn btn-primary btn-lg">
             Sign up
           </button>
         </div>
@@ -98,8 +107,12 @@ class SignupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
+    this.setState({ errors: {}, isLoading: true });
 
-    this.props.userSignupRequest(this.state);
+    this.props.userSignupRequest(this.state).then(
+      () => {},
+      ({ data }) => this.setState({ errors: data, isLoading: false })
+    );
   }
 
 }
