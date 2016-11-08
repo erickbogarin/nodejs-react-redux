461032853a862aae4983c8c9e73888d96f5f7abe
Erick Bogarin <erick.bogarin@gmail.com>
2016-11-08 14:53:18 -0400

    Add logout on client

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <Link to="/" className="navbar-brand">Red Dice</Link>
+import { logout } from '../actions/authActions';
+
+class NavigationBar extends React.Component {
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    const userLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#" onClick={this.logout.bind(this)}>Logout</a></li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="nav navbar-nav navbar-right">
+        <li><Link to="/signup">Sign up</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    );
+
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">Red Dice</Link>
+          </div>
+
+          <div className="collapse navbar-collapse">
+            { isAuthenticated ? userLinks : guestLinks }
+          </div>
+
         </div>
+      </nav>
+    );
+  }
 
-        <div className="collapse navbar-collapse">
-          <ul className="nav navbar-nav navbar-right">
-            <li><Link to="/signup">Sign up</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-        </div>
+  logout(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
 
-      </div>
-    </nav>
-  );
 }
+
+NavigationBar.propTypes = {
+  auth: React.PropTypes.object.isRequired,
+  logout: React.PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
